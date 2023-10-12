0b0b70a8ee8b5692f6837d0ed6e1c1a43de04908
Taures <taurest1988@gmail.com>
2023-10-12 19:59:51 +0300

    Курсовая работа № 7, добавил недостающие тесты

--- habit/tests.py
+++ habit/tests.py
@@ -100,23 +100,177 @@ class HabitCreateTest(APITestCase):
 
 
 
+class HabitPublicTest(APITestCase):
 
-    # def test_list_public_habit(self):
-    #     """ Тестирование вывода списка привычек c флагом публикации """
-    #
+    def setUp(self):
+        """Заполнение первичных данных"""
+
+        self.user = get_user_model().objects.create(
+            email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            role=UserRoles.owner,
+        )
+        self.user.set_password('Fynjybj88')
+        self.user.save()
+
+        token = RefreshToken.for_user(self.user)
+        self.access_token = str(token.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+
+        self.habit = Habit.objects.create(
+            place='Gym',
+            time="08:05:00",
+            action="train",
+            is_pleasant=True,
+            frequency=1,
+            execution_time="00:02:00",
+            is_publication=True,
+            owner=self.user
+        )
+    def test_list_public_habit(self):
+
+        """ Тестирование вывода списка привычек c флагом публикации """
+
+        response = self.client.get('/habit/public/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Habit.objects.all().count(), 1)
 
 
-    #
-    # def test_retrieve_habit(self):
-    #     """Тестирование вывода одной привычки """
+class HabitRetrieveTest(APITestCase):
 
-    #
-    #
-    # def test_update_put_habit(self):
-    #     """" Тестирование patch обновление привычки"""
-    #
+    def setUp(self):
+        """Заполнение первичных данных"""
 
-    #
-    # def test_destroy_habit(self):
-    #     """ Тестирование удаления привычки """
-    #
+        self.user = get_user_model().objects.create(
+            email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            role=UserRoles.owner,
+        )
+        self.user.set_password('Fynjybj88')
+        self.user.save()
+
+        token = RefreshToken.for_user(self.user)
+        self.access_token = str(token.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+
+        self.habit = Habit.objects.create(
+            place='Gym',
+            time="08:05:00",
+            action="train",
+            is_pleasant=True,
+            frequency=1,
+            execution_time="00:02:00",
+            is_publication=True,
+            owner=self.user
+        )
+
+    def test_retrieve_habit(self):
+
+        """Тестирование вывода одной привычки """
+
+        response = self.client.get(f'/habit/{self.habit.pk}/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class HabitUpdateTest(APITestCase):
+
+    def setUp(self):
+        """Заполнение первичных данных"""
+
+        self.user = get_user_model().objects.create(
+            email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            role=UserRoles.owner,
+        )
+        self.user.set_password('Fynjybj88')
+        self.user.save()
+
+        token = RefreshToken.for_user(self.user)
+        self.access_token = str(token.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+
+        self.habit = Habit.objects.create(
+            place='Gym',
+            time="08:05:00",
+            action="train",
+            is_pleasant=True,
+            frequency=1,
+            execution_time="00:02:00",
+            is_publication=True,
+            owner=self.user
+        )
+
+    def test_update_put_habit(self):
+        """" Тестирование patch обновление привычки"""
+
+        data = {
+            'owner': self.user.pk,
+            'place': 'Street',
+            'time': '12:40:00',
+            'action': 'walk',
+            'frequency': 1,
+            'execution_time': "00:02:00"
+        }
+
+        response = self.client.patch(f'/habit/update/{self.habit.pk}/',
+                                     data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()["place"], 'Street')
+
+
+class HabitDestroyTest(APITestCase):
+
+    def setUp(self):
+        """Заполнение первичных данных"""
+
+        self.user = get_user_model().objects.create(
+            email='taures_88@mail.ru',
+            first_name='Anton',
+            last_name='Turchenko',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
+            role=UserRoles.owner,
+        )
+        self.user.set_password('Fynjybj88')
+        self.user.save()
+
+        token = RefreshToken.for_user(self.user)
+        self.access_token = str(token.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+
+        self.habit = Habit.objects.create(
+            place='Gym',
+            time="08:05:00",
+            action="train",
+            is_pleasant=True,
+            frequency=1,
+            execution_time="00:02:00",
+            is_publication=True,
+            owner=self.user
+        )
+
+
+    def test_destroy_habit(self):
+        """ Тестирование удаления привычки """
+
+        response = self.client.delete(f'/habit/delete/{self.habit.pk}/')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Habit.objects.all().exists())
+        self.assertEqual(Habit.objects.all().count(), 0)
+
